96c7e1e150853664f6c965fe5e431ed3c92c8bd8
Renato Costa <renatocostatristao@gmail.com>
2023-09-07 14:43:37 -0300

    Mudanca na url para usar a refeerncia ao pod de event-bus

--- event-bus/index.js
+++ event-bus/index.js
@@ -11,10 +11,10 @@ app.post("/events",  (req, res) => {
   const event = req.body;
 
   events.push(event);
-  axios.post("http://localhost:4000/events", event).catch((err) => {
+  axios.post("http://posts-clusterip-srv:4000/events", event).catch((err) => {
     console.log(err.message);
   });
-   axios.post("http://localhost:4001/events", event).catch((err) => {
+   axios.post("http://comments-clusterip-srv:4001/events", event).catch((err) => {
     console.log(err.message);
   });
    axios.post("http://localhost:4002/events", event).catch((err) => {
