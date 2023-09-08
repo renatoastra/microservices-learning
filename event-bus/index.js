10d0960d5f106610a536e1a4484e73d2b09a1d7d
Renato <renatocostatristao@gmail.com>
2023-09-08 07:18:31 -0300

    configurando infra/skaffold

--- event-bus/index.js
+++ event-bus/index.js
@@ -17,10 +17,10 @@ app.post("/events",  (req, res) => {
    axios.post("http://comments-clusterip-srv:4001/events", event).catch((err) => {
     console.log(err.message);
   });
-   axios.post("http://localhost:4002/events", event).catch((err) => {
+   axios.post("http://query-clusterip-srv:4002/events", event).catch((err) => {
     console.log(err.message);
   });
-   axios.post("http://localhost:4003/events", event).catch((err) => {
+   axios.post("http://moderation-clusterip-srv:4003/events", event).catch((err) => {
     console.log(err.message);
   });
   res.send({ status: "OK" });
